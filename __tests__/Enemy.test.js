c95bcc2ccee085ab1ee271db85d18d4acdc9beb9
jsbdr <js.bdrx@gmail.com>
2022-01-03 20:33:46 -0600

    refactoring completed

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -1,43 +1,59 @@
-const Potion = require('./Potion.js');
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
 
-function Enemy(name, weapon) {
-  this.name = name;
-  this.weapon = weapon;
-  this.potion = new Potion();
+jest.mock('../lib/Potion.js');
 
-  this.health = Math.floor(Math.random() * 10 + 85);
-  this.strength = Math.floor(Math.random() * 5 + 5);
-  this.agility = Math.floor(Math.random() * 5 + 5);
-}
+test('creates an enemy object', () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-Enemy.prototype.getHealth = function() {
-  return `The ${this.name}'s health is now ${this.health}!`;
-};
+  expect(enemy.name).toBe('goblin');
+  expect(enemy.weapon).toBe('sword');
+  expect(enemy.health).toEqual(expect.any(Number));
+  expect(enemy.strength).toEqual(expect.any(Number));
+  expect(enemy.agility).toEqual(expect.any(Number));
+  expect(enemy.potion).toEqual(expect.any(Object));
+});
 
-Enemy.prototype.isAlive = function() {
-  if (this.health === 0) {
-    return false;
-  }
-  return true;
-};
+test("gets enemy's health value", () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-Enemy.prototype.getAttackValue = function() {
-  const min = this.strength - 5;
-  const max = this.strength + 5;
+  expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
 
-  return Math.floor(Math.random() * (max - min) + min);
-};
+test('gets a description of the enemy', () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-Enemy.prototype.reduceHealth = function(health) {
-  this.health -= health;
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
 
-  if (this.health < 0) {
-    this.health = 0;
-  }
-};
+test('checks if enemy is alive or not', () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-Enemy.prototype.getDescription = function() {
-  return `A ${this.name} holding a ${this.weapon} has appeared!`;
-};
+  expect(enemy.isAlive()).toBeTruthy();
 
-module.exports = Enemy;
+  enemy.health = 0;
+
+  expect(enemy.isAlive()).toBeFalsy();
+});
+
+test("gets enemy's attack value", () => {
+  const enemy = new Enemy('goblin', 'sword');
+  enemy.strength = 10;
+
+  expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("subtracts from enemy's health", () => {
+  const enemy = new Enemy('goblin', 'sword');
+  const oldHealth = enemy.health;
+
+  enemy.reduceHealth(5);
+
+  expect(enemy.health).toBe(oldHealth - 5);
+
+  enemy.reduceHealth(99999);
+
+  expect(enemy.health).toBe(0);
+});
